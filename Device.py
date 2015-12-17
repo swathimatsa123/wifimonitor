f683cd44c78c03c51b98d9270c24a77b350f9258
Swathi Matsa <matsa.swathi@gmail.com>
2015-12-16 21:59:56 -0800

    Added plotting and tested

--- Device.py
+++ Device.py
@@ -1,5 +1,5 @@
 class device:
-    def __init__(self, mac_addr, ip_addr):
+    def __init__(self, mac_addr, ip_addr=None):
         self.mac_address = mac_addr
         self.ip_address = ip_addr
         self.last_conn_time = 0
@@ -11,13 +11,16 @@ class device:
         
     def consume_bandwidth(self, byte_count):
         self.bandwidth += byte_count 
-        print 'I am in consume bandwidth'
 
     def get_bandwidth(self):
         return self.bandwidth
 
+    def reset_bandwidth(self):
+        self.bandwidth = 0
+
     def show_bandwidth(self):
-        print 'I am in show bandwidth'
+        pass
+        #print 'I am in show bandwidth'
 
 if __name__=="__main__":
     device = device(22,99)
--- Network.py
+++ Network.py
@@ -1,51 +1,97 @@
 import Device
+from scapy.all import *
+import threading
+from pylab import *
+import matplotlib.pyplot as plt
+import time
+import os
+
 class Network:
     num_devices = 0
     device_list = {}
+    bwUsageHistory = {}
 
-    def Join_device(self, mac_addr, ip_addr):
+    def Join_device(self, mac_addr, ip_addr=None):
         dev = Device.device(mac_addr, ip_addr)
         self.device_list[mac_addr] = dev 
-        print 'Join the device'
+        print 'Join the device %s' %(mac_addr)
+
+    def Does_Device_Exist (self, mac_addr) :
+        if self.device_list.has_key(mac_addr):
+            return True
+        else:
+            return False
 
     def Delete_device(self, mac_addr):
         del self.device_list[mac_addr]
         print 'Delete the device'
 
+    def plotUSage (self, sleepCount ) :
+        xdata = []
+        ydata = []
+        positions = []
+        count = 0
+        totalBandwidth = 0
+        for mac, device in self.device_list.items():
+            xdata.append(mac)
+            deviceBandwidth = device.get_bandwidth()
+            device.reset_bandwidth()
+            totalBandwidth = totalBandwidth + deviceBandwidth
+            ydata.append(deviceBandwidth)
+            positions.append(count+0.5)
+            count = count + 0.5
+
+        bar(positions,ydata, align='center')
+        xticks(positions, xdata)
+        ylabel('Bandwidth')
+        title('Total bandwidth usage in last 5 min')
+        grid(True)
+        savefig("usage5Min.png")
+        plt.clf()
+        print 'Plot image is ready. Name %s' %('usage5Min.png')
+        if (sleepCount % 6 == 0): 
+            print '## Printing total usage %s' %(totalBandwidth)
+            self.bwUsageHistory[sleepCount/6] = totalBandwidth
+            times = self.bwUsageHistory.keys()
+            totals = self.bwUsageHistory.values()
+            
+            plt.plot(times, totals)
+            grid(True)
+            os.system('/bin/mv totalUsage.png totalUsageSofar.png')
+            savefig("totalUsage.png")
+            plt.clf()
+        
+        
     def Display_devices(self):
-        print self.device_list
-        print 'Print all the devices in the network'
+        pass
 
     def Process_packet(self, pkt = None):
         for key, value in pkt.items():
-            print "Looking for device : %s" %key
-            print 'given bandwidth: %s' %value
             self.device_list[key].consume_bandwidth(value)
-            print self.device_list[key]
-        print 'Number of packets in the network'
 
-    def Print_bandwidth_statistics(self):
-        total_bandwidth = 0
-        for key, value in self.device_list.items():
-            total_bandwidth += value.get_bandwidth()
-            print 'Per device Bandwidth So far for %s is %s' %(key, value.get_bandwidth())
-        print 'Total Bandwidth So far : %s' %total_bandwidth  
-        
-
+def PktHeaderParser (pkt):
+    if pkt.haslayer(Dot11):
+        wrlsHdr = pkt[Dot11] 
+        dst = wrlsHdr.fields['addr1']
+        src = wrlsHdr.fields['addr2']
+        addr3 = wrlsHdr.fields['addr3']
+        if src != None:
+            if not network.Does_Device_Exist (src):
+                network.Join_device (src)
+            network.Process_packet({ src: len(pkt)})
+            
+def startMonitor():
+     sniff(iface="mon0", prn = PktHeaderParser)
+    
+global network
 if __name__=="__main__":
     network = Network()
-    network.Join_device("11:22:33:44:55:66", "192.168.42.1")
-    network.Join_device("aa:bb:33:44:55:66", "192.168.42.10")
-    network.Display_devices()
-    #
-    #  11:22:33:44:55:66 .,,,,
-    #  aa:bb: ...
-    #  
-    # network.Delete_device("11:22:33:44:55:66")
-    network.Display_devices()
-    #  aa:bb: ...
+    t=  threading.Thread(target=startMonitor)
+    t.start()
+    sleepCount = 0
+    while  True:
+        time.sleep(3)
+        sleepCount +=1
+        network.plotUSage(sleepCount)
+        
 
-    network.Process_packet({"11:22:33:44:55:66" : 1500})
-    network.Process_packet({"11:22:33:44:55:66" : 2500})
-    network.Process_packet({"aa:bb:33:44:55:66" : 3200})
-    network.Print_bandwidth_statistics()
